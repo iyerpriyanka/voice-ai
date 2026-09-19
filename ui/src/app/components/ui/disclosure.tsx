2db9b4fcb576e04b98324993bc534ab798354708
Priyanka Iyer <p_iyer@outlook.com>
2026-09-19 13:37:44 +0530

    refactor(ui): simplify disclosure panel

--- ui/src/app/components/ui/disclosure.tsx
+++ ui/src/app/components/ui/disclosure.tsx
@@ -1,28 +1,14 @@
-import React from 'react';
-import { Disclosure as DS, Transition } from '@headlessui/react';
+import type { ReactNode } from 'react';
 
 interface DisclosureProps {
   open: boolean;
-  children?: any;
+  children?: ReactNode;
 }
-export function Disclosure(props: DisclosureProps) {
-  return (
-    <DS defaultOpen={props.open}>
-      {({ open }) => (
-        <>
-          <Transition
-            show={props.open}
-            enter="transition duration-100 origin-top"
-            enterFrom="transform scale-y-0"
-            enterTo="transform scale-y-100 opacity-100"
-            leave="transition duration-100 origin-top"
-            leaveFrom="transform scale-y-100 opacity-100"
-            leaveTo="transform scale-y-0 opacity-0"
-          >
-            <DS.Panel static>{props.children}</DS.Panel>
-          </Transition>
-        </>
-      )}
-    </DS>
-  );
+
+export function Disclosure({ open, children }: DisclosureProps) {
+  if (!open) {
+    return null;
+  }
+
+  return <div>{children}</div>;
 }
